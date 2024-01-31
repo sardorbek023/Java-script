1df300f0e6fad66aaf8012f68e544086b3078156
sardorbek023 <ititschool4@gmail.com>
2024-01-31 16:55:54 +0500

    f

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-console.log('hello java script');
 
-console.log('salom');
+// console.log('loading....1');
 
-for (let index = 0; index < array.length; index++) {
-    
+// setTimeout(() => {
+//     console.log('salom');
+// },500)
+
+// setTimeout(() => {
+//     console.log('salom1');
+//     console.log('loading....3');
+// },0)
+// console.log('loading....2');
+
+// console.log('loading....2');
+// function fetchData(sardorbek) {
+//     setTimeout(() => {
+//         sardorbek();
+//         console.log("Data fetched!");
+//         console.log('loading....3');
+
+//     }, 2000);
+// }
+// console.log('loading....1');
+// function processData() {
+//     console.log("Data processed!");
+//     console.log('loading....4');
+
+// }
+// fetchData(processData);
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = false;
+            if (success) {
+                console.log("Data fetched!");
+                resolve("Data");
+            } else {
+                reject("Error fetching data");
+            }
+        }, 2000);
+    });
 }
+
+fetchData()
+    .then(data => console.log(`Processing ${data}`))
+    .catch(error => console.error(error));
+
+
+
+
+
+
+
+
